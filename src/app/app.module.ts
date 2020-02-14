6efe3aec28878c80f8dcb25234cd025bed92c6e8
nmsn <136696700@qq.com>
2020-02-14 21:16:45 +0800

    feat(tasks): 定时任务初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { UserModule } from '../user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HttpExceptionFilter } from '../common/filter/http-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TasksModule } from '../common/tasks/tasks.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_ADDRESS, { useNewUrlParser: true, useFindAndModify: false }),
+    ScheduleModule.forRoot(),
+    TasksModule,
     UserModule,
     RsshubModule,
     AuthModule,
